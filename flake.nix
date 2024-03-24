d5b9c7e18b4f9b39d7c9be4ce1efecf277bfaf94
Mihai Fufezan <fufexan@protonmail.com>
2024-03-24 14:30:05 +0200

    Nix: overhaul flake

--- flake.nix
+++ flake.nix
@@ -1,30 +1,44 @@
 {
   description = "Hyprpicker - a wlroots-compatible Wayland color picker that does not suck";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    systems.url = "github:nix-systems/default-linux";
+  };
 
   outputs = {
     self,
     nixpkgs,
+    systems,
     ...
   }: let
     inherit (nixpkgs) lib;
-    genSystems = lib.genAttrs [
-      # Add more systems if they are supported
-      "aarch64-linux"
-      "x86_64-linux"
-    ];
-    pkgsFor = nixpkgs.legacyPackages;
+    eachSystem = lib.genAttrs (import systems);
+    pkgsFor = eachSystem (system:
+      import nixpkgs {
+        localSystem.system = system;
+        overlays = with self.overlays; [hyprpicker];
+      });
     mkDate = longDate: (lib.concatStringsSep "-" [
       (builtins.substring 0 4 longDate)
       (builtins.substring 4 2 longDate)
       (builtins.substring 6 2 longDate)
     ]);
   in {
-    overlays.default = _: prev: rec {
-      hyprpicker = prev.callPackage ./nix/default.nix {
-        stdenv = prev.gcc12Stdenv;
-        version = "0.pre" + "+date=" + (mkDate (self.lastModifiedDate or "19700101")) + "_" + (self.shortRev or "dirty");
+    overlays = {
+      default = self.overlays.hyprpicker;
+      hyprpicker = lib.composeManyExtensions [
+        self.overlays.overrides
+        (final: prev: rec {
+          hyprpicker = final.callPackage ./nix/default.nix {
+            stdenv = final.gcc13Stdenv;
+            version = "0.pre" + "+date=" + (mkDate (self.lastModifiedDate or "19700101")) + "_" + (self.shortRev or "dirty");
+            inherit (prev.xorg) libXdmcp;
+          };
+          hyprpicker-debug = hyprpicker.override {debug = true;};
+        })
+      ];
+      overrides = final: prev: {
         wayland-protocols = prev.wayland-protocols.overrideAttrs (self: super: {
           version = "1.34";
           src = prev.fetchurl {
@@ -32,15 +46,14 @@
             hash = "sha256-xZsnys2F9guvTuX4DfXA0Vdg6taiQysAq34uBXTcr+s=";
           };
         });
-        inherit (prev.xorg) libXdmcp;
       };
-      hyprpicker-debug = hyprpicker.override {debug = true;};
     };
 
-    packages = genSystems (system:
-      (self.overlays.default null pkgsFor.${system})
-      // {default = self.packages.${system}.hyprpicker;});
+    packages = eachSystem (system: {
+      default = self.packages.${system}.hyprpicker;
+      inherit (pkgsFor.${system}) hyprpicker hyprpicker-debug;
+    });
 
-    formatter = genSystems (system: pkgsFor.${system}.alejandra);
+    formatter = eachSystem (system: pkgsFor.${system}.alejandra);
   };
 }
