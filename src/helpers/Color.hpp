500c46185dd4f2b5e16cd1a4edfe9ed1e126452e
Sepandar <116840915+flashrun24@users.noreply.github.com>
2025-05-18 16:03:24 +0000

    core: Make preview format match output format (#123)

--- src/helpers/Color.hpp
+++ src/helpers/Color.hpp
@@ -5,4 +5,7 @@
 class CColor {
   public:
     uint8_t r = 0, g = 0, b = 0, a = 0;
+    void    getCMYK(float& c, float& m, float& y, float& k) const;
+    void    getHSV(float& h, float& s, float& v) const;
+    void    getHSL(float& h, float& s, float& l) const;
 };
