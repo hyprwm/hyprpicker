0da95c0eda1a74180568aa074093ef87011d2fbb
Sepandar <116840915+flashrun24@users.noreply.github.com>
2026-05-05 23:02:03 +0000

    internal: fix app name for notifications (#158)

--- src/notify/Notify.cpp
+++ src/notify/Notify.cpp
@@ -13,7 +13,7 @@
 void NNotify::send(std::string hexColor, std::string formattedColor) {
     std::string             notifyBody = std::format("<span>Selected color: <span color='{}'><b>{}</b></span></span>", hexColor, formattedColor);
 
-    Hyprutils::OS::CProcess notify("notify-send", {"-t", "5000", "-i", "color-select-symbolic", "Color Picker", notifyBody});
+    Hyprutils::OS::CProcess notify("notify-send", {"-a", "hyprpicker", "-t", "5000", "-i", "color-select-symbolic", "Color Picker", notifyBody});
 
     notify.runAsync();
 }
